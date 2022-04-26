36908c078e6df7b7c27b17e6026d3e564009e80b
tmaker <you@example.com>
2022-04-26 23:52:56 +0800

    debug

--- project/project.go
+++ project/project.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/fatih/color"
 	"os"
 	"path"
+	"tf-cli/base"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -82,4 +84,57 @@ func run(cmd *cobra.Command, args []string) {
 			fmt.Fprintf(os.Stderr, "\033[31mERROR: Failed to create project(%s)\033[m\n", err.Error())
 		}
 	}
+}
+
+// Project is a project template.
+type Project struct {
+	Name string
+	Path string
+}
+
+// New new a project from remote repo.
+func (p *Project) New(ctx context.Context, dir,layout,branch string) error {
+	to := path.Join(dir, p.Name)
+	if _, err := os.Stat(to); !os.IsNotExist(err) {
+		fmt.Printf("🚫 %s already exists\n", p.Name)
+		override := false
+		prompt := &survey.Confirm{
+			Message: "📂 Do you want to override the folder ?",
+			Help:    "Delete the existing folder and create the project.",
+		}
+		e := survey.AskOne(prompt, &override)
+		if e != nil {
+			return e
+		}
+		if !override {
+			return err
+		}
+		os.RemoveAll(to)
+	}
+	fmt.Printf("🚀 Creating service %s, layout repo is %s, please wait a moment.\n\n", p.Name, layout)
+	repo := base.NewRepo(layout, branch)
+	if err := repo.CopyTo(ctx, to, p.Path, []string{".git", ".github"}); err != nil {
+		return err
+	}
+	// 忽略 cmd
+	//e := os.Rename(
+	//	path.Join(to, "cmd", "server"),
+	//	path.Join(to, "cmd", p.Name),
+	//)
+	//if e != nil {
+	//	return e
+	//}
+	base.Tree(to, dir)
+
+	fmt.Printf("\n🍺 Project creation succeeded %s\n", color.GreenString(p.Name))
+	fmt.Print("💻 Use the following command to start the project 👇:\n\n")
+
+	fmt.Println(color.WhiteString("$ cd %s", p.Name))
+	fmt.Println(color.WhiteString("$ go generate ./..."))
+	fmt.Println(color.WhiteString("$ go run main server"))
+	//fmt.Println(color.WhiteString("$ go build -o ./bin/ ./... "))
+	//fmt.Println(color.WhiteString("$ ./bin/%s -conf ./configs\n", p.Name))
+	fmt.Println("			🤝 Thanks for using tf-cli")
+	fmt.Println("	📚 Tutorial: https://github.com/tfcp/tf-cli")
+	return nil
 }
--- project/root.go
+++ project/root.go
@@ -28,13 +28,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tf-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
+	Long: `tf-cli is an interesting tools for build project template.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
